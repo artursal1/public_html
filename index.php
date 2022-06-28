4629f05ffdf9602964524713e17e6ff85648f1a4
artursal1 <salmanovartur@gmail.com>
2022-06-28 21:03:55 +0300

    master

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $obj = new View();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/96e181b2c4.js" crossorigin="anonymous"></script>
-    <title>MAGA</title>
+    <title>MAGAd</title>
     <link rel="stylesheet" type="text/css" href="css/main.css?<?php echo $rand; ?>">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </head>
